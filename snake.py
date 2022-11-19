a43ff4a488087e8a6fa1754a94d5e777356021ee
ShagizMan <danielgetaneh2011@gmail.com>
2022-11-19 12:01:58 +0300

    move the snake game

--- snake.py
+++ snake.py
@@ -5,16 +5,43 @@ dis=pygame.display.set_mode((600,500))
 
 pygame.display.set_caption('Snake Game by Daniel')
 
-blue = (0, 0, 255)
-red = (255, 0, 0)
+game_over = False
 
-game_over=False
+black = (0, 0, 0)
+red = (255, 0, 0)
+white = (255,  255, 255)
+
+x1 = 300
+y1 = 300
+x1_change = 0
+y1_change = 0
+
+clock = pygame.time.Clock()
+
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1_change = -10
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:
+                x1_change = 10
+                y1_change = 0
+            elif event.key == pygame.K_UP:
+                x1_change = 0
+                y1_change = -10
+            elif event.key == pygame.K_DOWN:
+                x1_change = 0
+                y1_change = 10
 
-    pygame.draw.rect(dis, red, [300, 200, 20, 20])
-    pygame.display.update() 
+    x1 += x1_change
+    y1 += y1_change
+    dis.fill(white)
+    pygame.draw.rect(dis, black, [x1, y1, 20, 20])
+    pygame.display.update()
+    clock.tick(30)
+
 pygame.quit()
 quit()
