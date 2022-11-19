090e49aa9cc81bdcf669a11e7e9cc5159d383c20
ShagizMan <danielgetaneh2011@gmail.com>
2022-11-19 11:36:18 +0300

    snake head created

--- snake.py
+++ snake.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 import pygame
 pygame.init()
-dis=pygame.display.set_mode((400,300))
-pygame.display.update()
+dis=pygame.display.set_mode((600,500))
+
 pygame.display.set_caption('Snake Game by Daniel')
+
+blue = (0, 0, 255)
+red = (255, 0, 0)
+
 game_over=False
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
- 
+
+    pygame.draw.rect(dis, red, [300, 200, 20, 20])
+    pygame.display.update() 
 pygame.quit()
 quit()
